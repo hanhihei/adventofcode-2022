44559e971f741785f70151bd3a30ba67d12483ed
Heikki Hanhijoki <heikki.hanhijoki@gmail.com>
2022-12-06 23:36:31 +0200

    solution for day 6

---
+++ 6/part2.py
@@ -0,0 +1,16 @@
+def main():
+    file = open('6.txt', 'r')
+    input = file.read()
+    startindex = 0
+    endindex = 14
+    for n in range(len(input)):
+        if len(set(input[startindex:endindex])) == len(input[startindex:endindex]):
+            print(endindex)
+            break
+        else:
+            startindex += 1
+            endindex += 1
+    
+
+if __name__ == "__main__":
+    main()
