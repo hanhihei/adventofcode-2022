a9bcac493a85c120ecaadde7e1b576e22344e04e
Heikki Hanhijoki <heikki.hanhijoki@gmail.com>
2022-12-06 16:08:51 +0200

    solution for day 4

---
+++ 4/part1.py
@@ -0,0 +1,26 @@
+def main():
+    containing_pairs = 0
+    file = open('4.txt', 'r')
+    Lines = file.readlines()
+    for line in Lines:
+        pair_split1 = line.strip().split(",")[0]
+        pair_split2 = line.strip().split(",")[1]
+        digit_split1 = pair_split1.split("-")
+        digit_split2 = pair_split2.split("-")
+        digit1 = int(digit_split1[0])
+        digit2 = int(digit_split1[1])
+        digit3 = int(digit_split2[0])
+        digit4 = int(digit_split2[1])
+        firstrange = range(digit1, digit2 + 1)
+        secondrange = range(digit3, digit4 + 1)
+        if len(firstrange) > len(secondrange):
+            if digit1 <= digit3 <= digit2 and digit1 <= digit4 <= digit2:
+                containing_pairs += 1
+        else:
+            if digit3 <= digit1 <= digit4 and digit3 <= digit2 <= digit4:
+                containing_pairs += 1
+    print(containing_pairs)
+
+
+if __name__ == "__main__":
+    main()
